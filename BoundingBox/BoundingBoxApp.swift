6f9f5e43e6996f64079ced49902ecd35ef3c9f46
littleossa <littleossa@gmail.com>
2022-04-07 10:06:03 +0900

    Initial Commit

---
+++ BoundingBox/BoundingBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoundingBoxApp.swift
+//  BoundingBox
+//
+//  Created by 平岡修 on 2022/04/07.
+//
+
+import SwiftUI
+
+@main
+struct BoundingBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
