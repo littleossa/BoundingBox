fa37802a4b4eb0953d291184dae5dbc4c39ba0d3
littleossa <littleossa@gmail.com>
2022-04-11 17:56:02 +0900

    [feat] Add feature to drag and move view

--- BoundingBox/BoundingBox.swift
+++ BoundingBox/BoundingBox.swift
@@ -7,58 +7,54 @@
 
 import SwiftUI
 
-struct BoundingBox<Content: View>: View {
+struct BoundingBox: View {
     
     @Binding var editingWidth: CGFloat
     @Binding var editingHeight: CGFloat
     @Binding var formType: EditFormType
+    @Binding var location: CGPoint
     @Binding var isEditing: Bool
-    private let content: Content
     
     init(editingWidth: Binding<CGFloat>,
          andHeight editingHeight: Binding<CGFloat>,
+         location: Binding<CGPoint>,
          formType: Binding<EditFormType>,
-         isEditing: Binding<Bool>,
-         @ViewBuilder _ content: () -> Content) {
+         isEditing: Binding<Bool>) {
         self._editingWidth = editingWidth
         self._editingHeight = editingHeight
+        self._location = location
         self._formType = formType
         self._isEditing = isEditing
-        self.content = content()
     }
     
     var body: some View {
         
-        VStack {
+        if isEditing {
             
             ZStack {
-                content
                 
-                if isEditing {
+                MovingDashFramedRectangle()
+                
+                EditPointsView { value in
                     
-                    MovingDashFramedRectangle()
-                    
-                    EditPointsView { value in
+                    switch formType {
+                    case .freeForm:
+                        guard editingWidth + value.scaleSize.width > 0 && editingHeight + value.scaleSize.height > 0
+                        else { return }
                         
-                        switch formType {
-                        case .freeForm:
-                            guard editingWidth + value.scaleSize.width > 0 && editingHeight + value.scaleSize.height > 0
-                            else { return }
-                            
-                            editingWidth += value.scaleSize.width
-                            editingHeight += value.scaleSize.height
-                            
-                        case .uniform:
-                            guard editingWidth + value.scaleValue > 0 && editingHeight + value.scaleValue > 0
-                            else { return }
-                            
-                            editingWidth += value.scaleValue
-                            editingHeight += value.scaleValue
-                        }
+                        editingWidth += value.scaleSize.width
+                        editingHeight += value.scaleSize.height
+                        
+                    case .uniform:
+                        guard editingWidth + value.scaleValue > 0 && editingHeight + value.scaleValue > 0
+                        else { return }
+                        
+                        editingWidth += value.scaleValue
+                        editingHeight += value.scaleValue
                     }
                 }
             }
-            .frame(width: editingWidth, height: editingHeight)
+            .position(location)
         }
     }
 }
@@ -67,9 +63,9 @@ struct BoundingBox_Previews: PreviewProvider {
     static var previews: some View {
         BoundingBox(editingWidth: .constant(100),
                     andHeight: .constant(100),
+                    location: .constant(CGPoint(x: 100,
+                                                y: 100)),
                     formType: .constant(.freeForm),
-                    isEditing: .constant(true)) {
-            Text("contents")
-        }
+                    isEditing: .constant(true))
     }
 }
--- BoundingBox/ContentView.swift
+++ BoundingBox/ContentView.swift
@@ -11,30 +11,44 @@ struct ContentView: View {
     
     @State private var width: CGFloat = 150
     @State private var height: CGFloat = 150
+    @State private var location: CGPoint = CGPoint(x: 150, y: 100)
     @State private var formType: EditFormType = .freeForm
     @State private var isEditing = true
+    
+    var dragGesture: some Gesture {
+        DragGesture().onChanged { value in
+            print("onChange")
+            print(location)
+            print(value.location)
+            location = value.location
+        }
+    }
     
     var body: some View {
         
         ZStack {
-            
             VStack {
                 Button {
                     isEditing.toggle()
                 } label: {
                     Text("Edit切り替え")
                 }
-
                 Spacer()
             }
             
-            BoundingBox(editingWidth: $width,
-                        andHeight: $height,
-            formType: $formType,
-            isEditing: $isEditing) {
+            Group {
                 Image(systemName: "circle")
                     .resizable()
+                    .position(location)
+                
+                BoundingBox(editingWidth: $width,
+                            andHeight: $height,
+                            location: $location,
+                            formType: $formType,
+                            isEditing: $isEditing)
             }
+            .frame(width: width, height: height)
+            .gesture(dragGesture)
             
             if isEditing {
                 EditOptionsView(formType: $formType)
