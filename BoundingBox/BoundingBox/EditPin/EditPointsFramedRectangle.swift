df03fadc7da5473da68802870a754030079dff67
littleossa <littleossa@gmail.com>
2022-10-03 04:40:18 +0900

    [refactor] modify comments in EditPointsFramedRectangle

--- BoundingBox/BoundingBox/EditPin/EditPointsFramedRectangle.swift
+++ BoundingBox/BoundingBox/EditPin/EditPointsFramedRectangle.swift
@@ -16,101 +16,96 @@ struct EditPointsFramedRectangle: View {
         ZStack {
             
             //
-            // ⬆️ Top edit points
-            //
             // Top Left Edit point
             //
-            //
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .topLeft).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .topLeading)
-                .offset(x: -5, y: -5)
-
-                
-                // Top Center Edit point
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .topLeft).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .top)
-                .offset(x: 0, y: -5)
-
-                
-                
-                // Top right Edit point
-                
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .topRight).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .topTrailing)
-                .offset(x: 5, y: -5)
-
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .topLeft).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .topLeading)
+            .offset(x: -5, y: -5)
             
             //
-            // ↔️ Middle edit points
+            // Top Center Edit point
             //
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .middleLeft).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .leading)
-                .offset(x: -5, y: 0)
-                
-                
-                
-                
-                
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .middleRight).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .trailing)
-                .offset(x: 5, y: 0)
-                        
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .topLeft).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .top)
+            .offset(x: 0, y: -5)
+            
+            
             //
-            // ⬇️ Bottom Edit points
+            // Top Right Edit point
             //
-           
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .bottomLeft).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .bottomLeading)
-                .offset(x: -5, y: 5)
-                
-                
-                
-                // Bottom Center
-                
-                
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .bottomCenter).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .bottom)
-                .offset(x: 0, y: 5)
-
-                
-                // Bottom Right
-                
-                
-                EditPoint { value in
-                    let scalingValue = EditPointScaling(with: value.translation,
-                                                        at: .bottomRight).value
-                    dragOnChangePinHandler(scalingValue)
-                }
-                .frame(width: width, height: height, alignment: .bottomTrailing)
-                .offset(x: 5, y: 5)
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .topRight).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .topTrailing)
+            .offset(x: 5, y: -5)
+            
+            
+            //
+            // Middle Left Edit point
+            //
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .middleLeft).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .leading)
+            .offset(x: -5, y: 0)
+            
+            
+            //
+            // Middle Right Edit point
+            //
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .middleRight).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .trailing)
+            .offset(x: 5, y: 0)
+            
+            //
+            // Bottom Left Edit point
+            //
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .bottomLeft).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .bottomLeading)
+            .offset(x: -5, y: 5)
+            
+            
+            //
+            // Bottom Center Edit point
+            //
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .bottomCenter).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .bottom)
+            .offset(x: 0, y: 5)
+            
+            //
+            // Bottom Right Edit point
+            //
+            EditPoint { value in
+                let scalingValue = EditPointScaling(with: value.translation,
+                                                    at: .bottomRight).value
+                dragOnChangePinHandler(scalingValue)
+            }
+            .frame(width: width, height: height, alignment: .bottomTrailing)
+            .offset(x: 5, y: 5)
         }
     }
 }
